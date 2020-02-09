05dc308ec222d469f9a06b94e80a0f69b37813b8
Frank Moran <info@francisfrankmoran.com>
2020-02-08 20:55:08 -0500

    add department and add role functionality

--- employee-tracker.js
+++ employee-tracker.js
@@ -1,6 +1,27 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "dbpw1234",
+    database: "employee_trackerDB"
+});
+
+connection.connect(function(err){
+    if (err) throw err;
+    // console.log("connected as id " + connection.threadId + "\n");
+    start();
+    // returnDepartments();
+});
+
 
 const start = () => {
     inquirer.prompt([
@@ -24,18 +45,118 @@ const start = () => {
             console.log("Adding Employee...");
         } 
         else if (answer === "Add Department") {
-            console.log("Adding Department...");
+            addDepartment();
         } 
         else if (answer === "Add Role") {
-            console.log("Adding Role...");
+            addRole();
         }
         else if (answer === "Update Employee Roles") {
             console.log("Updating Employee Roles...");
         }  
         else if (answer === "Quit") {
-            console.log("connection.end()");
+            console.log("Quitting application...");
+            connection.end();
         }  
     })
 }
 
-start();
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+
+        }
+    ]).then(response => {
+
+    });
+}
+
+// Helper function for when user wants to add a role
+const addRole = () => {
+    // Ask for title, salary, and department
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the title of the role?",
+            name: "title"
+        },
+        {
+            type: "number",
+            message: "What is the salary of the role?",
+            name: "salary"
+        },
+        {
+            type: "list",
+            message: "What department does this role belong to?",
+            choices: returnDepartments(),
+            name: "department"
+        }
+    ]).then(role => {
+        // Run query to obtain the department
+        // id of the department that the user chose
+        connection.query(`SELECT id FROM department WHERE (department.name = "${role.department}")`, function(err, res) {
+            if (err) throw err;
+            
+            // Run query to insert the title, salary,
+            // and the obtained department id into
+            // the role table
+            connection.query(
+                "INSERT INTO role SET ?",
+                {
+                    title: role.title,
+                    salary: role.salary,
+                    department_id: res[0].id
+                },
+                function(err) {
+                    if (err) throw err;
+                    console.log(`The role "${role.title}" has been added!`);
+                    console.log(`Role: ${role.title}  || Salary: ${role.salary} || Department: ${role.department}`);
+                    // send user back to start menu
+                    start();   
+                }
+            );
+
+        });
+        
+    });
+} 
+
+// Helper function to add a department
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of the department?",
+            name: "department"
+        }
+    ]).then(({ department }) => {
+        // Run query to insert the department that the
+        // user entered
+        connection.query(
+            "INSERT INTO department SET ?",
+            {
+                name: department
+            },
+            function(err) {
+                if (err) throw err;
+                console.log(`The department "${department}" has been added!`);
+                // send user back to start menu
+                start();
+              }
+        );
+    });
+}
+
+// Helper function to return a list of departments
+const returnDepartments = () => {
+    let departments = [];
+
+    connection.query("SELECT * FROM department", function(err, res) {
+        if (err) throw err;
+
+        res.forEach(department => {
+            departments.push(department.name);
+        });
+        // console.log(departments);
+    });
+    return departments;
+}
