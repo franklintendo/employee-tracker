2a1be658d52f1cfcece8911b9c85e5aee4e60178
Frank Moran <info@francisfrankmoran.com>
2020-02-10 01:35:13 -0500

    view employees by dept

--- employee-tracker.js
+++ employee-tracker.js
@@ -2,6 +2,9 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const cTable = require('console.table');
 
+const departments = [];
+const roles = [];
+
 var connection = mysql.createConnection({
     host: "localhost",
   
@@ -19,8 +22,34 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
     if (err) throw err;
     // console.log("connected as id " + connection.threadId + "\n");
+
+    
+    
+
+    // Grab existing departments
+    connection.query("SELECT name FROM department", function(err, res) {
+        if (err) throw err;
+        
+        res.forEach(department => {
+            departments.push(department.name);
+        });
+    });
+
+    // Grab existing roles
+    connection.query("SELECT title FROM role", function(err, res) {
+        if (err) throw err;
+        res.forEach(role => {
+            roles.push(role.title);
+        });
+    });
+
     start();
-    // viewAllEmployees();
+
+
+    // console.log(departments);
+    // returnDepartments();
+    // viewByDepartment();
+    // connection.end();
 });
 
 
@@ -37,7 +66,7 @@ const start = () => {
             viewAllEmployees();
         } 
         else if (answer === "View All Employees By Department") {
-            console.log("Viewing All Employees By Department...");
+            viewByDepartment();
         } 
         else if (answer === "View All Employees By Role") {
             console.log("Viewing All Employees By Role...");
@@ -77,7 +106,7 @@ const addEmployee = () => {
             type: "list",
             message: "What is the employee's role?",
             name: "role",
-            choices: returnRoles()
+            choices: roles
         }
         // {
         //     type: "list",
@@ -118,7 +147,7 @@ const addRole = () => {
         {
             type: "list",
             message: "What department does this role belong to?",
-            choices: returnDepartments(),
+            choices: departments,
             name: "department"
         }
     ]).then(role => {
@@ -141,6 +170,8 @@ const addRole = () => {
                     if (err) throw err;
                     console.log(`The role "${role.title}" has been added!`);
                     console.log(`Role: ${role.title}  || Salary: ${role.salary} || Department: ${role.department}`);
+                    // Update roles list
+                    roles.push(role.title);
                     // send user back to start menu
                     start();   
                 }
@@ -170,6 +201,8 @@ const addDepartment = () => {
             function(err) {
                 if (err) throw err;
                 console.log(`The department "${department}" has been added!`);
+                // Update departments list
+                departments.push(department);
                 // send user back to start menu
                 start();
               }
@@ -178,34 +211,31 @@ const addDepartment = () => {
 }
 
 // Helper function to return a list of departments
-const returnDepartments = () => {
-    let departments = [];
+// const returnDepartments = () => {
+//     let departments = [];
 
-    connection.query("SELECT * FROM department", function(err, res) {
-        if (err) throw err;
+//     connection.query("SELECT name FROM department", function(err, res) {
+//         if (err) throw err;
+        
+//         res.forEach(department => {
+//             departments.push(department.name);
+//         });
+//     });
+//     return departments;
+// }
 
-        res.forEach(department => {
-            departments.push(department.name);
-        });
-        // console.log(departments);
-    });
-    return departments;
-}
+// const returnRoles = () => {
+//     let roles = [];
 
-const returnRoles = () => {
-    let roles = [];
+//     connection.query("SELECT title FROM role", function(err, res) {
+//         if (err) throw err;
+//         res.forEach(role => {
+//             roles.push(role.title);
+//         });
+//     });
 
-    connection.query("SELECT title FROM role", function(err, res) {
-        if (err) throw err;
-        res.forEach(role => {
-            roles.push(role.title);
-            // console.log(role.title);
-        });
-        // console.log(roles);
-    });
-
-    return roles;
-}
+//     return roles;
+// }
 
 const employeeListForManager = (role) => {
     // Return employees of the role selected,
@@ -230,4 +260,34 @@ const viewAllEmployees = () => {
 
             start();
         });
+}
+
+const viewByDepartment = () => {
+
+    // let departments = returnDepartments();
+
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Choose a department to view their employees",
+            choices: departments,
+            name: "department"
+        }
+    ]).then(response => {
+        // console.log(`Viewing employees of ${response.department}`);
+        connection.query(
+            `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary
+            FROM ((department INNER JOIN role ON role.department_id = department.id) INNER JOIN employee ON employee.role_id = role.id) WHERE department.name = "${response.department}"`, 
+             function(err, res) {
+                // console.log(res);
+                if (err) throw err;
+                const table = cTable.getTable(res);
+                console.log("\n");
+                console.log(table);
+
+                start();
+             }
+        );
+    });
+
 }
--- seeds.sql
+++ seeds.sql
@@ -9,4 +9,14 @@ INSERT INTO role (title, salary, department_id)
 VALUES ("Engineer", 80000, 1), ("IT Intern", 7000, 1), ("Project Lead", 120000, 1), ("Senior Accountant", 120000, 2), ("Junior Accountant", 75000, 2), ("Accountant Intern", 3000, 2), ("Sales Lead", 120000, 3), ("Sales Person", 75000, 3), ("Sales Intern", 3000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jim", "Skanck", 3, null), ("Tim", "Poon", 1, 3), ("Carol", "Turd", 2, 3), ("Hank", "Rickles", 4, null), ("Rick", "Urine", 5, 4), ("Wayne", "Skipples", 6, 4), ("Samantha", "Vindle", 7, null), ("Greg", "Aguilera", 1, 7), ("Remy", "Yart", 2, 7);
+VALUES ("Jim", "Skanck", 3, null), ("Tim", "Poon", 1, 3), ("Carol", "Turd", 2, 3), ("Hank", "Rickles", 4, null), ("Rick", "Urine", 5, 4), ("Wayne", "Skipples", 6, 4), ("Samantha", "Vindle", 7, null), ("Greg", "Aguilera", 8, 7), ("Remy", "Yart", 9, 7);
+
+-- ### View all employees
+
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary
+FROM ((department INNER JOIN role ON role.department_id = department.id) INNER JOIN employee ON employee.role_id = role.id);
+
+-- ### View all employees by department
+
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary
+FROM ((department INNER JOIN role ON role.department_id = department.id) INNER JOIN employee ON employee.role_id = role.id) WHERE department.name = "IT";
